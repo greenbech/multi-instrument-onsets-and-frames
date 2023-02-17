e9f0fb1d76de44868bf9e3da301638d666197bc1
Håkon <haakon8855@users.noreply.github.com>
2023-02-17 12:53:35 +0100

    Add augmentations to pre-trainer and fix mnist_encoder

--- pre_training/mnist_encoder.py
+++ pre_training/mnist_encoder.py
@@ -1,7 +1,8 @@
 """haakoas, matsjno"""
 
 from torch import nn
-import torchvision
+import torch
+import numpy as np
 
 
 class MnistEncoder(nn.Module):
@@ -9,23 +10,36 @@ class MnistEncoder(nn.Module):
     Class contaning the Simple Siamese network.
     """
 
-    def __init__(self, layer_size):
+    def __init__(self):
         super().__init__()
         self.pre_trainer = nn.Sequential(
-            nn.Linear(layer_size, layer_size),
+            nn.Conv2d(in_channels=1, out_channels=4, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Linear(layer_size, layer_size),
+            nn.Conv2d(in_channels=4, out_channels=8, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Linear(layer_size, 10),
+            nn.Conv2d(in_channels=8, out_channels=4, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=4, out_channels=1, kernel_size=3, stride=1, padding=1),
             nn.Sigmoid(),
         )
 
     def forward(self, x):
-        x = x[:, None, :, :]  # Add empty dimension to act as number of channels
-        x = self.network(x)  # Expand number of channels to 3
-        x = self.resnet(x)
+        x = self.pre_trainer(x)
+        x = torch.flatten(x, 1)
         return x
 
+    def save(self, path: str):
+        torch.save(self.state_dict(), path)
 
-def main():
-    mnist = MnistEncoder(1000)
+
+def test():
+    mnist = MnistEncoder()
+    test_data = np.ones((5, 1, 229, 229))
+    test_data = torch.tensor(test_data).float()
+    x = mnist(test_data)
+    print(x)
+    print(x.shape)
+
+
+if __name__ == "__main__":
+    test()
--- pre_training/pre_trainer.py
+++ pre_training/pre_trainer.py
@@ -1,10 +1,13 @@
 """haakoas, matsjno"""
 
-from siamese_network import SiameseNetwork
 import torch
+import torchvision
 from torch import nn
-from torch.utils.data import DataLoader
 import numpy as np
+from torch.utils.data.dataloader import default_collate
+
+# from siamese_network import SiameseNetwork
+from mnist_encoder import MnistEncoder
 
 
 class PreTrainer:
@@ -13,9 +16,9 @@ class PreTrainer:
     """
 
     def __init__(self):
-        self.output_size = 1000
+        self.output_size = 28 * 28
         self.predictor_hidden_size = 2048
-        self.encoder = SiameseNetwork()
+        self.encoder = MnistEncoder()
         self.predictor = nn.Sequential(
             # Two fully connected layers creating a bottleneck
             nn.Linear(self.output_size, self.predictor_hidden_size),
@@ -30,25 +33,24 @@ class PreTrainer:
         x = self.encoder(test_data)
         print(x)
 
-    def train(self, epochs=10):
-        test_data = torch.tensor(np.random.rand(10, 229, 229)).float()
-        training_loader = DataLoader(test_data, batch_size=5, shuffle=True)
+    def train(self, training_loader, epochs=10):
+        # test_data = torch.tensor(np.random.rand(10, 229, 229)).float()
+        # training_loader = DataLoader(test_data, batch_size=5, shuffle=True)
         optimizer = torch.optim.SGD(
             self.encoder.parameters(),
-            lr=0.05,
+            lr=0.025,
             weight_decay=0.0001,
             momentum=0.9,
         )
         for i in range(epochs):
             running_loss = 0
             last_loss = 0
-            for j, data in enumerate(training_loader):
+            for j, (data, _) in enumerate(training_loader):
                 optimizer.zero_grad()
-                # TODO: Apply random augmentations
-                x1, x2 = data / 0.9, data * 0.9
+                x1, aug_idx = PreTrainer.rand_augment(data)
+                x2, _ = PreTrainer.rand_augment(data, avoid_idx=aug_idx)
 
                 z1, z2 = self.encoder(x1), self.encoder(x2)
-                print(z1)
                 p1, p2 = self.predictor(z1), self.predictor(z2)
 
                 loss = -(PreTrainer.cos_sim(p1, z2).mean() + PreTrainer.cos_sim(p2, z1).mean()) / 2
@@ -61,19 +63,70 @@ class PreTrainer:
                     print(f"Batch {j+1} Loss: {last_loss}")
                     running_loss = 0
 
+    def save(self, path: str):
+        self.encoder.save(path)
+
     @staticmethod
     def cos_sim(p, z):
         return torch.nn.CosineSimilarity(dim=1)(p, z)
+
+    @staticmethod
+    def rand_augment(data, avoid_idx=-1):
+        fill = -0.42421296
+        aug_idxs = [0, 1, 2, 3]
+        if avoid_idx in range(0, len(aug_idxs)):
+            aug_idxs.pop(avoid_idx)
+        rand_num = np.random.randint(0, len(aug_idxs))
+        aug_idx = aug_idxs[rand_num]
+        if aug_idx == 0:
+            # Blur image
+            augmenter = torchvision.transforms.GaussianBlur(kernel_size=5, sigma=(1.0, 2.0))
+            return augmenter(data), aug_idx
+        elif aug_idx == 1:
+            # Zoom in on image, keep dimensions
+            rand_crop = np.random.randint(20, 24)
+            augmenter = torchvision.transforms.CenterCrop(size=rand_crop)
+            resizer = torchvision.transforms.Resize(size=28)
+            return resizer(augmenter(data)), aug_idx
+        elif aug_idx == 2:
+            # Rotate image random angle
+            augmenter = torchvision.transforms.RandomRotation(degrees=(-90, 90), fill=fill)
+            return augmenter(data), aug_idx
+        elif aug_idx == 3:
+            # Apply random perspective to image
+            augmenter = torchvision.transforms.RandomPerspective(distortion_scale=0.5, fill=fill)
+            return augmenter(data), aug_idx
 
 
 def main():
     """
     Main function for running this python script.
     """
-    pre_trainer = PreTrainer()
-    pre_trainer.train()
-    # pre_trainer.test()
+    device = torch.device("cuda:0")
+
+    transform = torchvision.transforms.Compose(
+        [torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.1307,), (0.3081,))]
+    )
+
+    mnist_train = torchvision.datasets.MNIST("pre_training/data/", train=True, download=True, transform=transform)
+
+    indices = torch.arange(10000, 60000)
+    mnist_train = torch.utils.data.Subset(mnist_train, indices)
+
+    batch_size = 512
+    training_loader = torch.utils.data.DataLoader(
+        mnist_train,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=lambda x: tuple(x_.to(device) for x_ in default_collate(x)),
+    )
+
+    print(f"Cuda: {torch.cuda.is_available()}")
+    print(f"Device: {torch.cuda.get_device_name(0)}")
+
+    pre_trainer = PreTrainer().to(device)
+    pre_trainer.train(training_loader)
+    pre_trainer.save("mnist_encoder.pt")
 
 
-if __name__ == "__main__":
-    main()
+main()
